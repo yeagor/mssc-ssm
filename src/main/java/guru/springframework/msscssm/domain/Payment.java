f6feb0817eb6b4f76367456fdb322c8ae90ebd87
Georgy Vashchenko <gvashchenko@lineate.com>
2020-12-13 22:04:13 +0600

    Add Payment entity and State Machine configuration

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscssm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+
+    private BigDecimal amount;
+}
